53f841ccaa09868b54348b384122608572e23877
mohamedab125 <105331443+mohamedab125@users.noreply.github.com>
2022-08-27 16:29:22 +0100

    aaa

---
+++ src/main/java/com/backend/DevopsbootcampApplication.java
@@ -0,0 +1,13 @@
+package com.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsbootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsbootcampApplication.class, args);
+	}
+
+}
